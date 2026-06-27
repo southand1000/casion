51debe29a71842a94c6648ebccfb489e1c77afb4
southand1000 <southand.1000@gmail.com>
2026-06-27 12:25:19 +0900

    force build settings

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // ビルド時の型チェックを無効化する
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // ビルド時のeslintチェックを無効化する
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
